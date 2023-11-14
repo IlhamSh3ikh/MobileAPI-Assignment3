6c064b39ab3e74e1e698826216df40d6c711bf16
Ilham Sheikh <ilhamsheikh@Ilhams-MacBook-Pro.local>
2023-11-14 17:05:31 -0500

    Created Project files for iOS

---
+++ MobileAPI-Assignment3UITests/MobileAPI_Assignment3UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MobileAPI_Assignment3UITestsLaunchTests.swift
+//  MobileAPI-Assignment3UITests
+//
+//  Created by Ilham Sheikh on 2023-11-14.
+//
+
+import XCTest
+
+final class MobileAPI_Assignment3UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
